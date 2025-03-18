ad0a46909591ff226e819b6c436d0c453d1f4de3
adam vietro <hunterafi@gmail.com>
2025-03-17 18:42:36 -0700

    Added back the seeds.exs and added an ignore for the .env to the .gitignore

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,13 +10,13 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-# {:ok, user} =
-#   PicChat.Accounts.register_user(%{
-#     email: "test@test.test",
-#     password: "testtesttest"
-#   })
+{:ok, user} =
+  PicChat.Accounts.register_user(%{
+    email: "test@test.test",
+    password: "testtesttest"
+  })
 
 
-# for n <- 1..100 do
-#   PicChat.Messages.create_message(%{user_id: user.id, content: "message #{n}"})
-# end
+for n <- 1..100 do
+  PicChat.Messages.create_message(%{user_id: user.id, content: "message #{n}"})
+end
